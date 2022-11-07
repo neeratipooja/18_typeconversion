36aeb12c294796cb7e63d9c04e52ad4be3bab1f8
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-07 22:47:20 +0530

    Add files via upload

---
+++ typeconversion.py
@@ -0,0 +1,20 @@
+"""
+DAY:monday
+DATE:7th nov 2022
+TOPIC:strings
+AUTHOR:kiranmai
+"""
+b=""
+print(b.isspace())    #false
+b="   "
+print(b.isspace())     #true
+b="8"
+print(b.isspace())   #false
+a=input()    #given value 7
+b=input()   #given value 8
+print(a+b)   #78
+a=3
+b=4.2
+c="po"
+d="ki"   ; e=2; f="3.2"
+print(a+e) #5 in output
